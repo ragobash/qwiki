a3f089bcba71113b2bb0cc4ba81e227c1b61b674
Joshua R. Munoz <jrmunoz@Joshuas-MacBook-Pro.local>
2019-11-16 10:37:36 -0600

    qwiki card

---
+++ client/src/components/NavBar/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Button, AppBar, Toolbar } from "@material-ui/core";
+import LoginModal from "../LoginModal";
+import SignupModal from "../SignupModal";
+import SearchIcon from "@material-ui/icons/Search";
+import "./Navbar.css";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <AppBar position="static" className="navbar">
+          <Toolbar className="navbar">
+            <div className="btns">
+              <LoginModal />
+              <SignupModal />
+            </div>
+            <div className="search-box">
+              <input
+                type="text"
+                placeholder="Type to search"
+                className="search-txt"
+              ></input>
+              <a className="search-btn">
+                <SearchIcon />
+              </a>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
