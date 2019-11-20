c01e847d86fdc69ae992c1e48220ff67c3eed61b
Andrew Brooking <atbrooking.dev@gmail.com>
2019-11-19 18:47:48 -0600

    Code style standardization

--- client/src/components/BuilderToolbar/index.js
+++ client/src/components/BuilderToolbar/index.js
@@ -19,14 +19,34 @@
  */
 
 import React from "react";
-import Button from '../Button/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-function BuilderToolbar() {
-    return (
-        <div>
-        <Button />
-        </div>
-    );
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+function BuilderToolbar(props) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button onClick={props.onClick} sectionType='HEADING' variant="outlined" color="primary" className={classes.button}>
+        Heading
+      </Button>
+      <Button onClick={props.onClick} sectionType='IMAGE' variant="outlined" color="primary" className={classes.button}>
+        Image
+      </Button>
+      <Button onClick={props.onClick} sectionType='PARAGRAPH' variant="outlined" color="primary" className={classes.button}>
+        Paragraph
+      </Button>
+    </div>
+  );
 }
 
 export default BuilderToolbar;
--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,6 +1,25 @@
+/**
+ * 
+ * qWiki
+ * Copyright (C) 2019  Andrew Brooking, Josh Munoz, and Ryan Harris
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Button, AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar } from "@material-ui/core";
 import LoginModal from "../LoginModal";
 import SignupModal from "../SignupModal";
 import SearchIcon from "@material-ui/icons/Search";
