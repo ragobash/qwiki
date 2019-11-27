b2a9b403a177b1c27d95fd11aad1b50085a671f3
Joshua R. Munoz <jrmunoz@Joshuas-MacBook-Pro.local>
2019-11-26 19:35:13 -0600

    making 404 page

---
+++ client/src/pages/page404/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "../../components/Logo/index";
+class page404 extends React.Component {
+    render() {
+        return (
+            <div>
+                <Logo />
+                
+            </div>
+        );
+    }
+}
+
+export default page404;
