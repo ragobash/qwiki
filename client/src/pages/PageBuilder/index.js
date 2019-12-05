292214768c53773dd4b237438bc5fd033c123bc1
Andrew Brooking <atbrooking.dev@gmail.com>
2019-12-05 15:59:05 -0600

    More bug fixes

--- client/src/pages/PageBuilder/index.js
+++ client/src/pages/PageBuilder/index.js
@@ -120,11 +120,16 @@ class PageBuilder extends React.Component {
 
   sectionInput = event => {
     const index = event.target.parentNode.parentNode.getAttribute("data-index");
-    const content = event.target.parentNode.parentNode.value;
+    const content = event.target.value;
 
     let sections = [...this.state.sections];
 
-    sections[index].content = content;
+    sections[index] = {
+      sectionType: sections[index].sectionType,
+      content
+    };
+
+    console.log(sections);
 
     this.setState({
       sections
@@ -133,6 +138,8 @@ class PageBuilder extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
+
+    console.log(this.state.sections);
 
     API.newPage(this.state)
       .then(res => {
